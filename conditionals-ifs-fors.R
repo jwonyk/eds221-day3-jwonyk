d8bce5eabc86c183676e7df8f61aedd55a3b92e7
jwonyk <wonyoungkim@ucsb.edu>
2025-08-13 12:33:22 -0700

    time for food

--- conditionals-ifs-fors.R
+++ conditionals-ifs-fors.R
@@ -77,3 +77,74 @@ switch (species,
 )
 
 # see next week dplyr::case_when()!
+
+# Writing for loops
+
+dog_names <- c("Teddy", "Khorna", "Banjo", "Waffle")
+
+# create example for one data element to put into for loop
+# version with copy / pasting
+
+print(paste("My dog's name is", dog_names[1]))
+print(paste("My dog's name is", dog_names[2]))
+print(paste("My dog's name is", dog_names[3]))
+print(paste("My dog's name is", dog_names[4]))
+
+# version with a variable that I can update
+
+pupster <- 1
+
+print(paste("My dog's name is", dog_names[pupster]))
+
+# convert to a for loop
+
+for(pupster in dog_names) {
+  
+  print(paste("My dog's name is", pupster))
+  
+}
+
+# Another for loop example
+mass <- seq(from = 0, to = 3, by = 0.5)
+i <- mass[1]
+new_val <- i + 2 # adding 2 to each value
+print(new_val) # print the new value
+
+# write into a for loop
+for (i in 1:length(mass)) { # defining iterator using the length of vector
+  
+  new_val <- i + 2 # adding 2 to each value
+  print(new_val) # print the new value
+  
+}
+
+# same thing!
+for (i in seq_along(mass)) { # defining iterator using seq_along()
+  
+  new_val <- i + 2 # adding 2 to each value
+  print(new_val) # print the new value
+  
+}
+
+# another example with iterating by position (indexing)
+tree_height <- c(1, 2, 6, 10)
+
+# example for the first case
+tree_height[1] + tree_height[2]
+
+# convert into a generalizable expression
+i <- 1
+val <- tree_height[i] + tree_height[i + 1]
+print(val)
+
+# convert into a for loop
+# test out creating the sequence
+
+seq_along(tree_height)
+
+for(i in seq_along(tree_height)) {
+  
+  val <- tree_height[i] + tree_height[i + 1]
+  print(val)
+  
+}
