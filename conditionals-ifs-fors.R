2851e7c001a7c4f833d8d51e80e30f857224e435
jwonyk <wonyoungkim@ucsb.edu>
2025-08-13 15:07:15 -0700

    done with lecture

--- conditionals-ifs-fors.R
+++ conditionals-ifs-fors.R
@@ -148,3 +148,113 @@ for(i in seq_along(tree_height)) {
   print(val)
   
 }
+
+animal <- c("cat", "dog", "dog", "zebra", "dog")
+
+for(i in seq_along(animal)) {
+  
+  if(animal[i] == "dog") {
+    
+    print("I love dogs!")
+    
+  } else {
+    
+    print("These are other animals")
+    
+  }
+  
+}
+
+# Another for loop example with conditional statements
+
+# animal species
+species <- c("dog", "elephant", "goat", "dog", "dog", "elephant")
+
+# and their respective ages in human years
+age_human <- c(3, 8, 4, 6, 12, 18)
+
+# convert ages to "animal years" using the following:
+# 1 human year = 7 in dog years
+# 1 human year = 0.88 in elephant years
+# 1 human year =  4.7 in goat years
+
+# allocate space for our output
+animal_age <- vector(mode = "numeric", length = length(species))
+
+for(i in seq_along(species)) {
+  
+  if(species[i] == "dog") {
+    
+    animal_age <- age_human[i] * 7
+    
+  } else if(species[i] == "elephant") {
+    
+    animal_age <- age_human[i] * 0.88
+    
+  } else if(species[i] == "goat") {
+    
+    animal_age <- age_human[i] * 4.7
+    
+  }
+  
+  animal_age[i] <- animal_age # populate empty vector
+  
+}
+
+# Another example of storing an output
+tigers <- c(29, 34, 82)
+lions <- c(2, 16, 6)
+
+# allocate space for our output
+big_cats <- vector(mode = "numeric", length = length(tigers))
+
+for(i in seq_along(tigers)) {
+  
+  tocal_cats <- tigers[i] + lions[i]
+  
+}
+
+# for loops to iterate across of data frame
+
+# create vector to store values
+mean_mtcars <- vector(mode = "numeric", length = ncol(mtcars))
+
+for(i in 1:ncol(mtcars)) {
+  
+  mean_val <- mean(mtcars[[i]], na.rm = TRUE)
+  mean_mtcars[i] <- mean_val
+  
+}
+
+# A for loop over columns with a condition
+
+library(palmerpenguins)
+
+for(i in 1:ncol(penguins)) {
+  
+  if(is.numeric(penguins[[i]])) {
+    
+    penguin_median <- median(penguins[[i]], na.rm = TRUE)
+    print(penguin_median)
+    
+  } else {
+    
+    print("data not numeric")
+    
+  }
+  
+}
+
+# Functional programming
+
+# apply() function iterates over columns or rows
+
+# rewrite our for loop for finding mean values of the columns in mtcars
+apply(X = mtcars, MARGIN = 2, FUN = mean)
+# same as saying apply the function mean across the column in mtcars
+
+library(tidyverse)
+
+penguins %>%
+  group_by(species) %>%
+  summarise(across(where(is.numeric), mean, na.rm = TRUE))
